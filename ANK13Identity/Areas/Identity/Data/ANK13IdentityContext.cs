70c3a207a05997084a6058f75e18c6c6b3f1f2e3
haybe <haybe@EWDOD>
2023-08-24 16:47:08 +0300

    Add project files.

---
+++ ANK13Identity/Areas/Identity/Data/ANK13IdentityContext.cs
@@ -0,0 +1,22 @@
+﻿using ANK13Identity.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ANK13Identity.Areas.Identity.Data;
+
+public class ANK13IdentityContext : IdentityDbContext<ANK13IdentityUser>
+{
+    public ANK13IdentityContext(DbContextOptions<ANK13IdentityContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
